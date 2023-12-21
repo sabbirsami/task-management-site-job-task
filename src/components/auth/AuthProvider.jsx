e4190fb6db6f33047b74401221ed52b8bac080c7
SabbirSami <smd71430@gmail.com>
2023-12-21 13:12:10 +0600

    add sign in create account system

--- src/components/auth/AuthProvider.jsx
+++ src/components/auth/AuthProvider.jsx
@@ -1,6 +1,12 @@
 import PropTypes from "prop-types";
 import { createContext, useState } from "react";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {
+    signInWithPopup,
+    GoogleAuthProvider,
+    signOut,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+} from "firebase/auth";
 import auth from "../../../firebase.config";
 
 export const AuthContext = createContext(null);
@@ -8,12 +14,30 @@ const AuthProvider = ({ children }) => {
     const [loading, setLoading] = useState(true);
     const googleProvider = new GoogleAuthProvider();
     const user = false;
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+    const signInUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+    const signOutUser = () => {
+        setLoading(true);
+        return signOut(auth);
+    };
 
     const signInWithGoogle = () => {
         setLoading(true);
         return signInWithPopup(auth, googleProvider);
     };
-    const data = { user, signInWithGoogle };
+    const data = {
+        user,
+        signInWithGoogle,
+        signInUser,
+        createUser,
+        signOutUser,
+    };
     return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
 };
 
